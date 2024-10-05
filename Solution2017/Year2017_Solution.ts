71c654eadfb9d57eed5e9fba4c305cd4984483c7
Erwan <eb.web.account@icloud.com>
2024-10-05 13:08:29 +0530

    **feature: Implement Day 1**
    _Commit Description:_
    
    Implement solution for both parts of day 1.
    
    _Associated Change Request:_
    [CR #004]

--- Solution2017/Year2017_Solution.ts
+++ Solution2017/Year2017_Solution.ts
@@ -38,4 +38,67 @@ export class Year2017_Solution
             return `Solution for year 2017, day ${day}, part ${isFirstPart ? "one" : "two"} has not yet been developed.`;
         }
     }
+
+    /*
+     * Get solution for day 1 "Inverse Captcha", Part 1.
+     *  https://adventofcode.com/2017/day/1
+     *
+     * @returns: Captcha to proove Santa is not a human.
+     */
+    private static day_01_Part_1(): number
+    {
+        // Get puzzle input
+        let inputOfExercise: string = ReadFile.getLine(1);
+
+        // Value of the captcha.
+        let solutionOfCaptcha: number = 0;
+        
+        // Iterating among all pairs that are side by side.
+        for (let index: number = 1; inputOfExercise.length > index; ++index)
+        {
+            // If values are equal, then the value should be added to the captcha.
+            if (inputOfExercise[index] == inputOfExercise[index - 1])
+            {
+                solutionOfCaptcha += Number(inputOfExercise[index]);
+            }
+        }
+        
+        // It's a circular string, so the first and the last may match.
+        if (inputOfExercise[0] == inputOfExercise[inputOfExercise.length - 1])
+        {
+            solutionOfCaptcha += Number(inputOfExercise[0]);
+        }
+
+        // Return the computed captcha.
+        return solutionOfCaptcha;
+    }
+
+    /*
+     * Get solution for day 1 "Inverse Captcha", Part 2.
+     *  https://adventofcode.com/2017/day/1#part2
+     *
+     * @returns: Second captcha to proove Santa is not a human.
+     */
+    private static day_01_Part_2(): number
+    {
+        // Get puzzle input
+        let inputOfExercise: string = ReadFile.getLine(1);
+
+        // Value of the captcha.
+        let solutionOfCaptcha: number = 0;
+
+        // Iterating among all pairs halfway around. No need to iterate two times for checking the same values.
+        for (let index: number = 0; (inputOfExercise.length / 2) > index; ++index)
+        {
+            // If values are equal, then the value should be added 2 times to the captcha:
+            // One time for index, the other for the halfwaray around.
+            if (inputOfExercise[index] == inputOfExercise[index + (inputOfExercise.length / 2)])
+            {
+                solutionOfCaptcha += 2 * Number(inputOfExercise[index]);
+            }
+        }
+
+        // Return the computed captcha.
+        return solutionOfCaptcha;
+    }
 }
--- Solution2017/main.ts
+++ Solution2017/main.ts
@@ -2,7 +2,8 @@ import { Year2017_Solution } from "./Year2017_Solution.js";
 
 function main(): void
 {
-    console.log("To see solutions of year 2017 in TypeScript that are not merged yet, look at branch 'feature-004_Develop_2017_Solution'.");
+    console.log(Year2017_Solution.getSolution(1, true));
+    console.log(Year2017_Solution.getSolution(1, false));
 }
 
 main();
