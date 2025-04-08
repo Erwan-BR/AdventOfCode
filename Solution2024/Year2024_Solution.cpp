74603ad098b01444b45dc7d27274a26e6bcd0b1c
Erwan <eb.web.account@icloud.com>
2025-04-08 19:54:09 +0200

    **feature: Implement Day 1**
    _Commit description:_
    
    Implement solution for both parts of day 1.
    
    _Associated Change Request:_
    [CR #024]

--- Solution2024/Year2024_Solution.cpp
+++ Solution2024/Year2024_Solution.cpp
@@ -14,7 +14,7 @@ long long Year2024_Solution::getSolution(const unsigned int day, const bool isFi
     // Table for method of part 1
     static long long (*firstPartTable[])() = {
         nullptr,
-        nullptr,
+        &day_01_Part_1,
         nullptr,
         nullptr,
         nullptr,
@@ -44,7 +44,7 @@ long long Year2024_Solution::getSolution(const unsigned int day, const bool isFi
     // Table for method of part 2
     static long long (*secondPartTable[])() = {
         nullptr,
-        nullptr,
+        &day_01_Part_2,
         nullptr,
         nullptr,
         nullptr,
@@ -100,4 +100,106 @@ long long Year2024_Solution::getSolution(const unsigned int day, const bool isFi
 
     // Call the method for the second part
     return (*secondPartTable[day])();
+}
+
+/*
+* Search for the distance between first and second list.
+*
+* @returns: Total distance the two given lists.
+*/
+long long Year2024_Solution::day_01_Part_1()
+{
+    // Retrieve puzzle input
+    const std::vector<std::string> linesOfInput = ReadFile::getLines(1);
+
+    // Regex used to find both numbers.
+    const std::regex numberRegex(R"(\d+)");
+
+    // Create two priority queue that will store value in the order so we don't have to sort them at the end.
+    std::priority_queue<int> valueCol1;
+    std::priority_queue<int> valueCol2;
+
+    // Creating iterators to use regex
+    std::sregex_iterator words_begin;
+
+    const std::sregex_iterator words_end = std::sregex_iterator();
+
+    // Iterating among all lines.
+    for (const std::string line: linesOfInput)
+    {
+        // Search for the start of the regex.
+        words_begin = std::sregex_iterator(line.begin(), line.end(), numberRegex);
+
+        std::sregex_iterator indexIterator = words_begin;
+        
+        // Add the first value converted as an integer.
+        valueCol1.push(std::stoi((*indexIterator).str()));
+
+        ++ indexIterator;
+
+        // Add the second value converted as an integer.
+        valueCol2.push(std::stoi((*indexIterator).str()));
+    }
+
+    long long result = 0;
+
+    // Compute the difference between each biggest value and delete them from the total result.
+    while (! valueCol1.empty())
+    {
+        result += abs(valueCol1.top() - valueCol2.top());
+
+        valueCol1.pop();
+        valueCol2.pop();
+    }
+
+    return result;
+}
+
+/*
+* Compute the similarity score of the two given lists.
+*
+* @returns: Similarity score of the two given lists.
+*/
+long long Year2024_Solution::day_01_Part_2()
+{
+    // Creating iterators to use regex
+    const std::vector<std::string> linesOfInput = ReadFile::getLines(1);
+
+    // Regex used to find both numbers.
+    const std::regex numberRegex(R"(\d+)");
+
+    // Create two unordered map that will store values and their occurences.
+    std::unordered_map<unsigned int, unsigned int> valueCol1;
+    std::unordered_map<unsigned int, unsigned int> valueCol2;
+
+    std::sregex_iterator words_begin;
+
+    const std::sregex_iterator words_end = std::sregex_iterator();
+
+    // Iterating among all lines.
+    for (const std::string line: linesOfInput)
+    {
+        // Search for the start of the regex.
+        words_begin = std::sregex_iterator(line.begin(), line.end(), numberRegex);
+
+        std::sregex_iterator indexIterator = words_begin;
+        
+        // Increment the number of time the value has been found in the first list.
+        ++ valueCol1[std::stoi((*indexIterator).str())];
+
+        ++ indexIterator;
+
+        // Increment the number of time the value has been found in the second list.
+        ++ valueCol2[std::stoi((*indexIterator).str())];
+    }
+
+    long long result = 0;
+
+    // Compute the similarty of the given lists.
+    for (const std::pair<unsigned int, unsigned int>& pairOfVal: valueCol1)
+    {
+        result += pairOfVal.first * valueCol2[pairOfVal.first];
+    }
+
+    return result;
 }
--- Solution2024/Year2024_Solution.hpp
+++ Solution2024/Year2024_Solution.hpp
@@ -2,6 +2,9 @@
 #define _YEAR2024_SOLUTION_HPP_
 
 #include <iostream>
+#include <regex>
+#include <unordered_map>
+#include <queue>
 
 #include "ReadFile.hpp"
 
@@ -9,6 +12,10 @@ class Year2024_Solution
 {
     public:
         static long long getSolution(const unsigned int day, const bool isFirstPart);
+    
+    private:
+        static long long day_01_Part_1();
+        static long long day_01_Part_2();
 };
 
 #endif // _YEAR2024_SOLUTION_HPP_
